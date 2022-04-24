b13e33f8dc626816c4a532e32900d4a49a3912ef
Romber Li <romber2001@163.com>
2022-04-24 10:39:52 +0800

    init commit

--- pkg/message/message.go
+++ pkg/message/message.go
@@ -17,6 +17,7 @@ package message
 
 import (
 	"github.com/romberli/go-util/config"
+	"github.com/romberli/go-util/constant"
 )
 
 const DefaultMessageHeader = "GTW"
@@ -30,5 +31,12 @@ func init() {
 
 // NewMessage returns *config.ErrMessage with specified values
 func NewMessage(code int, values ...interface{}) *config.ErrMessage {
+	if len(values) > constant.ZeroInt {
+		err, ok := values[constant.ZeroInt].(error)
+		if ok {
+			return Messages[code].Renew(values[1:]...).SetError(err)
+		}
+	}
+
 	return Messages[code].Renew(values...)
 }
