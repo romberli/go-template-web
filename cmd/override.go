cadb986882ecab765b7ff4cc19b2272d303a07c8
Romber Li <romber2001@163.com>
2023-02-21 11:22:07 +0800

    init commit

---
+++ cmd/override.go
@@ -0,0 +1,129 @@
+package cmd
+
+import (
+	"github.com/pingcap/errors"
+	"github.com/romberli/go-template-web/config"
+	"github.com/romberli/go-template-web/pkg/message"
+	"github.com/romberli/go-util/constant"
+	"github.com/spf13/cast"
+	"github.com/spf13/viper"
+	"strings"
+)
+
+// OverrideConfigByCLI read configuration from command line interface, it will override the config file configuration
+func OverrideConfigByCLI() error {
+	// override config
+	overrideConfigByCLI()
+	// override daemon
+	err := overrideDaemonByCLI()
+	if err != nil {
+		return err
+	}
+	// override log
+	err = overrideLogByCLI()
+	if err != nil {
+		return err
+	}
+	// override server
+	err = overrideServerByCLI()
+	if err != nil {
+		return err
+	}
+	// validate configuration
+	err = config.ValidateConfig()
+	if err != nil {
+		return message.NewMessage(message.ErrValidateConfig, err)
+	}
+
+	return nil
+}
+
+// overrideConfigByCLI overrides the config section by command line interface
+func overrideConfigByCLI() {
+	if cfgFile != constant.EmptyString && cfgFile != constant.DefaultRandomString {
+		viper.Set(config.ConfKey, cfgFile)
+	}
+}
+
+// overrideDaemonByCLI overrides the daemon section by command line interface
+func overrideDaemonByCLI() error {
+	if daemonStr != constant.DefaultRandomString {
+		daemon, err := cast.ToBoolE(daemonStr)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		viper.Set(config.DaemonKey, daemon)
+	}
+
+	return nil
+}
+
+// overrideLogByCLI overrides the log section by command line interface
+func overrideLogByCLI() error {
+	if logFileName != constant.DefaultRandomString {
+		viper.Set(config.LogFileNameKey, logFileName)
+	}
+	if logLevel != constant.DefaultRandomString {
+		logLevel = strings.ToLower(logLevel)
+		viper.Set(config.LogLevelKey, logLevel)
+	}
+	if logFormat != constant.DefaultRandomString {
+		logLevel = strings.ToLower(logFormat)
+		viper.Set(config.LogFormatKey, logFormat)
+	}
+	if logMaxSize != constant.DefaultRandomInt {
+		viper.Set(config.LogMaxSizeKey, logMaxSize)
+	}
+	if logMaxDays != constant.DefaultRandomInt {
+		viper.Set(config.LogMaxDaysKey, logMaxDays)
+	}
+	if logMaxBackups != constant.DefaultRandomInt {
+		viper.Set(config.LogMaxBackupsKey, logMaxBackups)
+	}
+	if logRotateOnStartupStr != constant.DefaultRandomString {
+		rotateOnStartup, err := cast.ToBoolE(logRotateOnStartupStr)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		viper.Set(config.LogRotateOnStartupKey, rotateOnStartup)
+	}
+
+	return nil
+}
+
+// overrideServerByCLI overrides the server section by command line interface
+func overrideServerByCLI() error {
+	if serverAddr != constant.DefaultRandomString {
+		viper.Set(config.ServerAddrKey, serverAddr)
+	}
+	if serverPidFile != constant.DefaultRandomString {
+		viper.Set(config.ServerPidFileKey, serverPidFile)
+	}
+	if serverReadTimeout != constant.DefaultRandomInt {
+		viper.Set(config.ServerReadTimeoutKey, serverReadTimeout)
+	}
+	if serverWriteTimeout != constant.DefaultRandomInt {
+		viper.Set(config.ServerWriteTimeoutKey, serverWriteTimeout)
+	}
+	if serverPProfEnabledStr != constant.DefaultRandomString {
+		pprofEnabled, err := cast.ToBoolE(serverPProfEnabledStr)
+		if err != nil {
+			return errors.Trace(err)
+		}
+
+		viper.Set(config.ServerPProfEnabledKey, pprofEnabled)
+	}
+	if serverRouterAlternativeBasePath != constant.DefaultRandomString {
+		viper.Set(config.ServerRouterAlternativeBasePathKey, serverRouterAlternativeBasePath)
+	}
+	if serverRouterAlternativeBodyPath != constant.DefaultRandomString {
+		viper.Set(config.ServerRouterAlternativeBodyPathKey, serverRouterAlternativeBodyPath)
+	}
+	if serverRouterHTTPErrorCode != constant.DefaultRandomInt {
+		viper.Set(config.ServerRouterHTTPErrorCodeKey, serverRouterHTTPErrorCode)
+	}
+
+	return nil
+}
--- cmd/root.go
+++ cmd/root.go
@@ -18,18 +18,15 @@ package cmd
 
 import (
 	"fmt"
-	"os"
-	"path/filepath"
-	"strings"
-
 	"github.com/pingcap/errors"
 	"github.com/romberli/go-template-web/config"
 	"github.com/romberli/go-template-web/pkg/message"
 	"github.com/romberli/go-util/constant"
 	"github.com/romberli/log"
-	"github.com/spf13/cast"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
+	"os"
+	"path/filepath"
 )
 
 const defaultConfigFileType = "yaml"
@@ -212,84 +209,6 @@ func ReadConfigFile() (err error) {
 	}
 
 	return nil
-}
-
-// OverrideConfigByCLI read configuration from command line interface, it will override the config file configuration
-func OverrideConfigByCLI() (err error) {
-	// override config
-	if cfgFile != constant.EmptyString && cfgFile != constant.DefaultRandomString {
-		viper.Set(config.ConfKey, cfgFile)
-	}
-
-	// override daemon
-	if daemonStr != constant.DefaultRandomString {
-		daemon, err := cast.ToBoolE(daemonStr)
-		if err != nil {
-			return errors.Trace(err)
-		}
-
-		viper.Set(config.DaemonKey, daemon)
-	}
-
-	// override log
-	if logFileName != constant.DefaultRandomString {
-		viper.Set(config.LogFileNameKey, logFileName)
-	}
-	if logLevel != constant.DefaultRandomString {
-		logLevel = strings.ToLower(logLevel)
-		viper.Set(config.LogLevelKey, logLevel)
-	}
-	if logFormat != constant.DefaultRandomString {
-		logLevel = strings.ToLower(logFormat)
-		viper.Set(config.LogFormatKey, logFormat)
-	}
-	if logMaxSize != constant.DefaultRandomInt {
-		viper.Set(config.LogMaxSizeKey, logMaxSize)
-	}
-	if logMaxDays != constant.DefaultRandomInt {
-		viper.Set(config.LogMaxDaysKey, logMaxDays)
-	}
-	if logMaxBackups != constant.DefaultRandomInt {
-		viper.Set(config.LogMaxBackupsKey, logMaxBackups)
-	}
-	if logRotateOnStartupStr != constant.DefaultRandomString {
-		rotateOnStartup, err := cast.ToBoolE(logRotateOnStartupStr)
-		if err != nil {
-			return errors.Trace(err)
-		}
-
-		viper.Set(config.LogRotateOnStartupKey, rotateOnStartup)
-	}
-
-	// override server
-	if serverAddr != constant.DefaultRandomString {
-		viper.Set(config.ServerAddrKey, serverAddr)
-	}
-	if serverPidFile != constant.DefaultRandomString {
-		viper.Set(config.ServerPidFileKey, serverPidFile)
-	}
-	if serverReadTimeout != constant.DefaultRandomInt {
-		viper.Set(config.ServerReadTimeoutKey, serverReadTimeout)
-	}
-	if serverWriteTimeout != constant.DefaultRandomInt {
-		viper.Set(config.ServerWriteTimeoutKey, serverWriteTimeout)
-	}
-	if serverPProfEnabledStr != constant.DefaultRandomString {
-		pprofEnabled, err := cast.ToBoolE(serverPProfEnabledStr)
-		if err != nil {
-			return errors.Trace(err)
-		}
-
-		viper.Set(config.ServerPProfEnabledKey, pprofEnabled)
-	}
-
-	// validate configuration
-	err = config.ValidateConfig()
-	if err != nil {
-		return message.NewMessage(message.ErrValidateConfig, err)
-	}
-
-	return err
 }
 
 // UsageTemplateWithoutDefault returns a usage template which does not contain default part
